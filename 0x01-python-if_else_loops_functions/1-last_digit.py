3870297662015b7020a12823a2b1a6ce82285fd1
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-10 19:37:44 +0000

    pep8 corrections

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,10 @@ elif number > 0:
 str = "Last digit of"
 
 if last_d > 5:
-    print("{} {} is {} and is greater than 5".format(str, number, last_d))
+    print("{:s} {:d} is {:d} and is\
+ greater than 5".format(str, number, last_d))
 elif last_d == 0:
-    print("{} {} is {} and is 0".format(str, number, last_d))
+    print("{:s} {:d} is {:d} and is 0".format(str, number, last_d))
 elif last_d < 6 and last_d != 0:
-    print("{} {} is {} and is less than 6 and not 0".format(str, number, last_d))
+    print("{:s} {:d} is {:d} and is\
+ less than 6 and not 0".format(str, number, last_d))
