dc711b107e4adeae83bada3ec30c71d515b3491f
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-10 19:19:59 +0000

    'Task 1, modulus negative number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    last_d = number % -10
+elif number > 0:
+    last_d = number % 10
+
+
+if last_d > 5:
+    print("Las digit of {} is {} and is greater than 5".format(number, last_d))
+elif last_d == 0:
+    print("Las digit of {} is {} and is 0".format(number, last_d))
+elif last_d < 6 and last_d != 0:
+    print("Las digit of {} is {} and is less than 6 and not 0".format(number, last_d))
