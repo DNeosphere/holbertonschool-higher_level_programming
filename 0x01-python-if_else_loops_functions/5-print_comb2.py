fd2b110438d86c6cd993774090a8e29c0bb74511
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-10 20:22:26 +0000

    Print numbers with patdding

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for num in range (100):
-    print("{:02d}, ".format(num), end='')
+for num in range(100):
     if num == 99:
         print("{:02d}".format(num))
+        break
+    print("{:02d}, ".format(num), end='')
