95ee423e09c7a6c1b3f71bcc89224d345f9d2155
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-10 20:44:49 +0000

    Task 6, print numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for first_num in range (0, 9):
-    for sec_num in range (1, 10):
+for first_num in range(0, 9):
+    for sec_num in range(1, 10):
         if first_num == 8 and sec_num == 9:
             print("{}{}".format(first_num, sec_num))
             break
-        if first_num == sec_num:
+        if first_num == sec_num or first_num > sec_num:
             continue
         print("{}{}, ".format(first_num, sec_num), end='')
