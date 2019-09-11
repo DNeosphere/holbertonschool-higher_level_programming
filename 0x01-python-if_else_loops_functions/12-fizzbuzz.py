7afa1e0ac522a31832c34abde64121b1d4aa4f58
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-11 00:46:45 +0000

    FizzBuzz algo with python

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+
+    for i in range(1, 101):
+        if i == 100:
+            print("Buzz ", end='')
+            return
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end='')
+        if i % 3 == 0:
+            print("Fizz ", end='')
+        if i % 5 == 0:
+            print("Buzz ", end='')
+        else:
+            print("{} ".format(i), end='')
