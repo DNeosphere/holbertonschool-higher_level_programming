93c1f48e7f0137d5e91f9074f86c2b41c3bd0867
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-12 19:06:02 +0000

    Task 2, import sys to use argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    len_argv = len(argv)
+
+    if len_argv == 1:
+        print("0 arguments")
+        exit()
+    elif len_argv == 2:
+        print("1 argument:\n1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len_argv - 1))
+
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
