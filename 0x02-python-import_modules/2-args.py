f676d1f756a57f057e496a3d695a082ee2ed26c3
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-12 19:10:31 +0000

    Typo corrected

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("0 arguments.")
         exit()
     elif len_argv == 2:
-        print("1 argument: \n1: {}".format(argv[1]))
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len_argv - 1))
 
