a6c32c732c41e0c588a3c311baa6377c353e9f30
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-13 01:31:56 +0000

    Typo fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
+
     argv_len = len(argv)
 
     if argv_len != 4:
-        print("Usage ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(argv[1])
