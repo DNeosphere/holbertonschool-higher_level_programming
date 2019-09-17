253abbbf9c43d788c6752957e803f1b9a7790076
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-17 19:39:38 +0000

    new line fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        return None
-    i_in = 0
-    i_out = 0
-    for i_out in range(len(matrix)):
-        for i_in in range(len(matrix[i_out])):
-            if i_in < len(matrix)-1:
-                print("{:d}".format(matrix[i_out][i_in]), end=' ')
-            elif i_in == len(matrix)-1:
-                print("{:d}".format(matrix[i_out][i_in]))
+        print()
+    else:
+        i_in = 0
+        i_out = 0
+        for i_out in range(len(matrix)):
+            for i_in in range(len(matrix[i_out])):
+                if i_in < len(matrix)-1:
+                    print("{:d} ".format(matrix[i_out][i_in]), end='')
+                else:
+                    print("{:d}".format(matrix[i_out][i_in]), end='')
+            print()
