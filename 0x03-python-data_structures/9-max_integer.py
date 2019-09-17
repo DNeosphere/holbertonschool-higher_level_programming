15323794daa98d6921155b5ea986bb02054837e4
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-17 21:22:01 +0000

    find the max integer of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    compare = 0
+    compare = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] >= compare:
             compare = my_list[i]
