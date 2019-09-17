68fc100092f3998454a20fbe21e64aa7d248b36f
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-17 21:05:45 +0000

    Multiple returns in python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     len_sentence = len(sentence)
     if len_sentence is None:
-        char = None
+        char = "None"
     else:
         char = sentence[0]
     return len_sentence, char
