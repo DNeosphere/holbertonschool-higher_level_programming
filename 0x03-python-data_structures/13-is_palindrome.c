1060027f1b0a44592a8557d6f9d3e4632a46ea89
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-18 00:18:02 +0000

    Extra function deleted

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,49 +6,33 @@
  */
 int is_palindrome(listint_t **head)
 {
-	size_t list_le = list_len(*head);
 	listint_t *temp = *head;
-	size_t count = 0, iter = 0;
+	size_t count = 0, len = 0, iter = 0;
 	int buffer[1024];
 
 	if (head == NULL)
-		return (1);
+		return (0);
 
-	if (list_le == 0 || list_le == 1)
-	{
-		return (1);
-	}
 	while (temp)
 	{
 		buffer[count] = temp->n;
 		count++;
 		temp = temp->next;
 	}
+	len = count -1;
 
-	while (iter <= (list_le - 1))
+	if (len <= 1)
+		return (1);
+
+	while (iter <= (len))
 	{
-		if (buffer[iter] != buffer[list_le - 1])
+		if (buffer[iter] != buffer[len])
 			return (0);
 		iter++;
-		list_le--;
+		len--;
 	}
 	return (1);
 
 
 }
-/**
- * list_len - prints the number of elements of a list
- * @h: pointer to the list
- * Return: number of elements
- */
-size_t list_len(const listint_t *h)
-{
-	size_t count = 0;
 
-	while (h)
-	{
-		h = h->next;
-		count++;
-	}
-	return (count);
-}
