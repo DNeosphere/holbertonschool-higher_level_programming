31d3804d368fe15b2238f7ce8d16c3c0f82aa555
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-20 01:14:59 +0000

    Best score, max fx

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     maxim = max(zip(a_dictionary.values(), a_dictionary.keys()))
-    if maxim is None:
+    if maxim is 0:
         return None
     return maxim[1]
