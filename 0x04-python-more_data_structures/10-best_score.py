329d67c13403ba5206f2d79b0672513ed5dffa35
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-20 01:11:16 +0000

    Best score, max fx

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,3 @@ def best_score(a_dictionary):
         return None
     maxim = max(zip(a_dictionary.values(), a_dictionary.keys()))
     return maxim[1]
-    #    key = max(a_dictionary.keys(), key=(lambda k: a_dictionary[k]))
- #   return key
