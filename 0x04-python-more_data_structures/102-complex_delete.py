a93628820cc90f3d729e7d22060a368319f9efe6
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-21 00:45:43 +0000

    delete from a dictionary

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+
+    if value not in a_dictionary:
+        return a_dictionary
+
+    for i in list(a_dictionary.keys()):
+        if a_dictionary[i] == value:
+            del a_dictionary[i]
+    return a_dictionary
