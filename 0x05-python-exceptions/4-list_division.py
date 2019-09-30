fff826fba42f3b365b4d53a723cd0bed4127b4a2
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-30 19:45:18 +0000

    Task 4, try/except/finally

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    n_list = [0] * list_length
+    c = 0
+    for i in range(list_length):
+        try:
+            c = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            c = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            c = 0
+        except IndexError:
+            print("out of range")
+        finally:
+            n_list[i] = c
+    return n_list
