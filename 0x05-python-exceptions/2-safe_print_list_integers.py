b8a6884ccf54f91bd3cd823916c21ba2b15766e5
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-30 18:50:07 +0000

    Error tuple, task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-            count =+ 1
+            count += 1
         except (TypeError, ValueError):
             pass
     print()
