222099d4f2dc23e107f3eee8758d3b5c85ca1612
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-30 18:02:47 +0000

    Fix feature: EXcept

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in my_list:
+    count = 0
+    for i in range(x):
         try:
-            print("{}".format(i), end='')
+            print("{}".format(my_list[i]), end='')
+            count += 1
         except:
             pass
-        if x == i:
-            break
     print()
-    return i
+    return count
