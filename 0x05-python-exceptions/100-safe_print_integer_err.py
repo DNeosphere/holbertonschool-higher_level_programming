9f50210f874d628c5e7b3fcdebd89e5d715ccfe7
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-30 20:32:39 +0000

    Print to stderr, fixed varible Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError):
-        print("Exception: ", file=sys.stderr)
+    except Exception as ex_err:
+        print("Exception: {}".format(str(ex_err)), file=sys.stderr)
         return False
     return True
