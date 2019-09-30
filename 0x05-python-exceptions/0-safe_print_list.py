8f99c5bc000ce84ce08ceb7810cb8035cd50657e
DNeosphere <daniela.gomezp05@outlook.com>
2019-09-30 17:58:06 +0000

    Fix feature: EXcept

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in my_list:
+    for i in my_list:
+        try:
             print("{}".format(i), end='')
-            if x == i:
-                break
-        print()
-
-    except:
-        pass
-
+        except:
+            pass
+        if x == i:
+            break
+    print()
     return i
