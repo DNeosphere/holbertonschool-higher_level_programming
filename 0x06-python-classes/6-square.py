83034044a552f590403950131295644d427b3468
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-01 23:25:24 +0000

    Validations added

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,21 +20,6 @@ class Square:
             raise ValueError('size must be >= 0')
         self.__size = value
 
-    def my_print(self):
-        if self.__size <= 0:
-            print()
-
-        for i in range(self.__position[1]):
-            print()
-
-        for i in range(self.__size):
-            for j in range(self.__size + self.__position[0]):
-                if j < self.__position[0]:
-                    print(' ', end='')
-                else:
-                    print('#', end='')
-            print()
-
     @property
     def position(self):
         return self.__position
@@ -50,3 +35,18 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
 
         self.__position = value
+
+    def my_print(self):
+        if self.__size <= 0:
+            print()
+
+        for i in range(self.__position[1]):
+            print()
+
+        for i in range(self.__size):
+            for j in range(self.__size + self.__position[0]):
+                if j < self.__position[0]:
+                    print(' ', end='')
+                else:
+                    print('#', end='')
+            print()
