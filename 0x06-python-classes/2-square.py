9961a3569117e97a4d65ab9b52ee6c20698b0399
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-01 21:30:20 +0000

    Assignation at the end

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size = 0):
-        self._size = size
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
 
         if size < 0:
             raise ValueError('size must be >= 0')
+
+        self.__size = size
