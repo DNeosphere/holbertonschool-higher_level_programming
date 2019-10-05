897533f25ef1323bb67fb016ebfaf8349e96ee49
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-05 21:54:31 +0000

    Fix: indentantion and pep8

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_square(size):
-	if not isinstance(size, int):
-		raise TypeError('size must be an integer')
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
 
-	if size < 0:
-		raise TypeError('size must be >= 0')
+    if size < 0:
+        raise TypeError('size must be >= 0')
 
-	for i in range(size):
-		for j in range(size):
-			print('#', end='')
-		print()
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+    print()
