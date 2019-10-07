59666c8815339b347021ffa4b519dcb5892cabc4
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-07 18:28:41 +0000

    Feature: task 0 and 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Definition of a rectangle class
+"""
+
+
+class Rectangle:
+    """ Rectangle definition """
+    def __init__(self, width=0, height=0):
+        """
+        Initialization of the instances
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(width):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+
+        if width < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = width
+
+    @property
+    def height(height):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
