b76cb0cef575579d5180fcc4c6f5b118554c5ebb
Daniela Gomez <41307089+DNeosphere@users.noreply.github.com>
2019-10-07 20:54:21 -0500

    Update 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -67,7 +67,4 @@ class Rectangle:
 
     def perimeter(self):
         """ Returns the rectangle perimeter """
-        if self.__height == 0 or self.__width == 0:
-            return 0
-
         return (self.__width * 2) + (self.__height * 2)
