d1f6e6e8cc83e396c13bc5ea9fb57a44506fbd65
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-15 20:55:41 +0000

    Fixing else return

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,3 +2,5 @@
 def is_same_class(obj, a_class):
     if type(obj) is a_class:
         return True
+    else:
+        return False
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,3 +2,5 @@
 def is_kind_of_class(obj, a_class):
     if isinstance(obj, a_class):
         return True
+    else:
+        return False
