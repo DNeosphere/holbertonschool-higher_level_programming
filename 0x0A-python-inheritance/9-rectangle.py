4fc9b96a7c80124f58ef120b2da38f53b397f052
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-15 23:54:58 +0000

    Task 9, area method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+""" Class inheritance """
+
+
+class Rectangle(BaseGeometry):
+    """ Example of inheritance """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
