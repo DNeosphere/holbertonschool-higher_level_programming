5c28975578b46b334e0906f072579082120b3a9f
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-16 02:20:51 +0000

    Task 11, str and super() functions

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+""" Class inheritance """
+
+
+class Square(Rectangle):
+    """ defines a Square """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Finds the area of a square, using inheritance of rectangle """
+        return super().area()
+
+    def __str__(self):
+        """ Returns a string with description of the function """
+        return "[Square] {}/{}".format(self.__size, self.__size)
