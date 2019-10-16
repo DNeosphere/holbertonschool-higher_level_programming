97196961fe7f6855c030fed33459876f459f2dc2
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-16 22:56:43 +0000

    Fix: dict

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -11,11 +11,17 @@ class Student:
 
     def to_json(self, attrs=None):
         self_dict = self.__dict__
+        comp = 0
+
         if type(attrs) == list:
-            n_dict = {}
-            for item in self_dict.keys():
-                if item in attrs:
-                    n_dict[item] = self_dict[item]
-            return n_dict
+            for item in attrs:
+                if type(item) == str:
+                    comp += 1
+            if len(attrs) == comp:
+                n_dict = {}
+                for item in self_dict.keys():
+                    if item in attrs:
+                        n_dict[item] = self_dict[item]
+                return n_dict
         else:
             return self_dict
