9e9dfe05c3e7c4c22c612663a18cf06277cc9ddf
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-16 02:10:32 +0000

    Fix: condition greater or equal to cero

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,5 +11,5 @@ class BaseGeometry:
         """ Integer validation """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value < 0:
+        if value <= 0:
             raise ValueError("{}  must be greater than 0".format(name))
