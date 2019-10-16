e77cf88b39c9da3436ad7ca9702162e139e3670c
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-16 21:42:39 +0000

    Task 11: definition of the class Student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" DEfinition of a class """
+
+
+class Student:
+    """ Defines a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
