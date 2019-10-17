79a6ed6862884e23a07400e0afa1e4335d651ac1
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-17 01:34:21 +0000

    Pascal task

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -3,10 +3,10 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    pascal = [[1], [1, 1]]
+    pascal = [[1]]
     n_list = []
 
-    for row in range(2, n):
+    for row in range(1, n):
         n_list = [1]
         for line in range(1, row):
             elem = pascal[row - 1][line - 1] + pascal[row - 1][line]
