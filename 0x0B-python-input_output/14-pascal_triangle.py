9b0017a8c49dc8b9d80743c2e59cf1cece341749
DNeosphere <daniela.gomezp05@outlook.com>
2019-10-17 01:39:49 +0000

    Fix: return when n == 1

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -3,10 +3,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    pascal = [[1]]
+    if n == 1:
+        return [[1]]
+
+    pascal = [[1], [1, 1]]
     n_list = []
 
-    for row in range(1, n):
+    for row in range(2, n):
         n_list = [1]
         for line in range(1, row):
             elem = pascal[row - 1][line - 1] + pascal[row - 1][line]
