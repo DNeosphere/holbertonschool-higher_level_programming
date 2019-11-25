58f0253cbec78dd73dcef301aa8cb189a99d9112
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-25 22:53:46 +0000

    Feature: change to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,17 @@
+-- Convert a database to UTF-8
+ALTER DATABASE
+    hbtn_0c_0
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+
+ALTER TABLE
+    hbtn_0c_0.first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE
+    hbtn_0c_0.first_table
+    CHANGE name name
+    VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
