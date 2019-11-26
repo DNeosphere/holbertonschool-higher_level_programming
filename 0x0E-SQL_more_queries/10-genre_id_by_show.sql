6670d06c4d942ff3829d30ad859243b3a547b1e8
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 22:44:38 +0000

    Task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Inner Join
 SELECT shows.title, show_gen.genre_id
-FROM tv_shows as shows
-INNER JOIN tv_show_genres as show_gen
+FROM tv_shows AS shows
+INNER JOIN tv_show_genres AS show_gen
 ON shows.title = show_gen.show_id
 ORDER BY shows.title, show_gen.genre_id;
