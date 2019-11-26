04fb9876e80aa10e1546d2a3b79bc0ad1caa5a7c
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 20:59:12 +0000

    Fix: slect

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
 --  lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT id, name FROM hbtn_0d_usa.cities
-WHERE hbtn_0d_usa.cities.id = (
-SELECT hbtn_0d_usa.states WHERE name = 'California'
-);
+SELECT id, name FROM cities
+WHERE cities.id = (
+      SELECT state_id
+      FROM states
+      WHERE name = 'California'
+      );
