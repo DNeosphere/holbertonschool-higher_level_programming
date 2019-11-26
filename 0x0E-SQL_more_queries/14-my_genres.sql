752b644a487688f6a416305e0338409b1414fbae
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 23:47:18 +0000

    Feature: doiuble join

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- two joins
+SELECT tv_genres.name
+FROM tv_show_genres
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter';
