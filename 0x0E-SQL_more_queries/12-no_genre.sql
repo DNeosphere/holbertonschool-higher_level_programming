a170ed30f1c14b285bd3c7d08c308f95351aa774
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 23:04:30 +0000

    Feature: Where  clause

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Left join
+SELECT shows.title, show_gen.genre_id
+FROM tv_shows AS  shows
+LEFT JOIN tv_show_genres AS show_gen
+ON shows.id = show_gen.show_id
+WHERE show_gen.genre_id IS NULL
+ORDER BY shows.title, show_gen.genre_id;
