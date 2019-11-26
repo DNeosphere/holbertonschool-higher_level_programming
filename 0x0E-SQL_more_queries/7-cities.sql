d636698556c2ecfcb4cad8e331a7f07dced3af51
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 20:26:36 +0000

    Feature: Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+state_id NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+name VARCHAR(256) NOT NULL
+);
