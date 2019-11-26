d99e0eb27c0240a2cb5a32997530ed26f7abfdba
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 22:46:56 +0000

    Fix: wrong key

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT shows.title, show_gen.genre_id
 FROM tv_shows AS shows
 INNER JOIN tv_show_genres AS show_gen
-ON shows.title = show_gen.show_id
+ON shows.id = show_gen.show_id
 ORDER BY shows.title, show_gen.genre_id;
