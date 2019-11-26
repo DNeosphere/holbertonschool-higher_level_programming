ded4b31c064d30315d44121b153df41230f3f950
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 23:50:28 +0000

    Fix. order by added

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,4 @@ ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+ORDER BY tv_genres.name ASC;
