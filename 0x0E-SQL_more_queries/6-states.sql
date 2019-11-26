3252b8b29887fe62109cc387a8297935939d4f62
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 19:45:23 +0000

    Feature: create states table, primary key id, autogenerated

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+--  creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256)
+);
