7646b8cb98fd9928e44ab2db91cb055fd8c80cfc
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-26 20:33:43 +0000

    Fix: typo

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
 state_id NOT NULL,
 FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
 name VARCHAR(256) NOT NULL
