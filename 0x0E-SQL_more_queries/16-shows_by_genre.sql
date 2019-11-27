5b50659b9b41cef2cc0f214a085ca281ea68342c
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-27 00:10:30 +0000

    Feature: last task YAAAH

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- JEJE
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
