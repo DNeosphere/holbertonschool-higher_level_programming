3558b8cbcfe5e1f13e09d11b504d1dfe2c125e00
DNeosphere <daniela.gomezp05@outlook.com>
2019-11-27 00:23:49 +0000

    Fix: right join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- JEJE
 SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
-LEFT JOIN tv_genres
+RIGHT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-LEFT JOIN tv_shows
+RIGHT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_genres.name ASC;
