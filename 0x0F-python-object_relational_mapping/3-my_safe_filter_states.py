727c677453a79a8e0d030f754b6b752717de6f9c
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-13 20:00:33 +0000

    FEature: safe parameters

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Selects all states from a db
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="127.0.0.1", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states \
+    WHERE name = %(state_name)s \
+    ORDER BY states.id ASC;", {'state_name': argv[4]})
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
