6f39dd7de516d4e10c0146fc3e79802fc1e5233a
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-13 23:14:00 +0000

    Feautre: task 7, use of ORM, NO MOAR SQL

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+creates a base class
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print("{} {}".format(instance.id, instance.name))
