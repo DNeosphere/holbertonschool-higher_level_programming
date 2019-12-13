32cabffd5282a0f8b76ef3705f6d15f976b9fc49
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-13 19:51:05 +0000

    Fix: port set

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1],
+    db = MySQLdb.connect(host="127.0.0.1", port=3306,  user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
