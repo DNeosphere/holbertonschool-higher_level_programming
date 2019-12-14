7a5a5984ec95bdff6c25387c63c63cafa98e51a5
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-14 23:52:22 +0000

    Feture: task 8, print first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+creates a base class
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first = session.query(State).first()
+    if first is None:
+        print(Nothing)
+    else:
+        print("{}: {}".format(first.id, first.name))
