b96652f9d9302b94d3bc7de45dbd234a96056a76
Daniela Gomez <41307089+DNeosphere@users.noreply.github.com>
2019-12-14 18:25:57 -0500

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states \
-    WHERE name = '{:s}' \
+    WHERE name  LIKE BINARY '{}' \
     ORDER BY states.id ASC;".format(argv[4]))
     states = cur.fetchall()
 
