250a5e78f89cae4d46bfd60a09eb9072a9773272
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-15 22:42:11 +0000

    FEature: task 14, JOIN

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+creates a base class
+"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state, city in session.query(State, City).\
+        filter(State.id == City.state_id).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
