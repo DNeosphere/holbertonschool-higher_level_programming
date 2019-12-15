c15ea714507c6ee1001bfecb12b39f26990f2f42
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-15 16:42:22 +0000

    fix: indentation

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(State.name.like('%a%').\
-                                             filter_by(State.id)):
+    for state in session.query(State).filter(State.name.like('%a%')
+                                             .filter_by(State.id).all()):
         session.delete(state)
     session.commit()
     session.close()
