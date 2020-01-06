0b012de4d2bf361ec5783da52c055cfeb59d7bdf
Daniela Gomez <41307089+DNeosphere@users.noreply.github.com>
2020-01-06 13:13:55 -0500

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept.
-curl -sI $1 | grep Allow | cut -d ":" -f 2 | cut -d " " -f 2
+curl -sI $1 | grep Allow | cut -d ":" -f 2 | cut -c 2-
