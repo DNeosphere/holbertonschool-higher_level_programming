01f7f29cb55782e86b1e6f62ee600f48aca28f0b
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-06 17:34:01 +0000

    Fix: argv

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
-curl -sI http://1e0a6b7ef075.20.hbtn-cod.io:5000 | grep Content-Length | cut -d ":" -f 2 | cut -d " " -f 2
+curl -sI $1 | grep Content-Length | cut -d ":" -f 2 | cut -d " " -f 2
