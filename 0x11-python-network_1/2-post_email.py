99f8220cbd818e956b339af6eb48d7584e793a0e
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-13 18:43:19 +0000

    Featire: task 2, not finished, POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from urllib import parse
+from urllib import request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {'email': argv[2]}
+    data = parse.urlencode(value)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page)
