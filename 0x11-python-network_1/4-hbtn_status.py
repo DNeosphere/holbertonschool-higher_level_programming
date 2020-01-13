e30d2f0812d4052d6f5bfb1a83d0c3c9df1a9732
dneosphere <daniela.gomezp05@outlook.com>
2020-01-13 19:36:15 +0000

    Feature: task 4, response with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(req.text)))
-    print('\t- content {}'.format(req.text))
+    print('\t- content: {}'.format(req.text))
