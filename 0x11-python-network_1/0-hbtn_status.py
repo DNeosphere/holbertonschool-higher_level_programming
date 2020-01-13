f1f93789f5e8ccaad52ff52124f748d6de4275dc
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-13 17:56:18 +0000

    Fix: tab at the beginning

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         utf = content.decode('utf-8')
-        print('- type: {}'.format(type(content)))
-        print('- content: {}'.format(content))
-        print('- utf8 content: {}'.format(content.decode('utf-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf8 content: {}'.format(content.decode('utf-8')))
