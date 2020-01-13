e9392557bd300daa2663e31941d8d44bd544796b
dneosphere <daniela.gomezp05@outlook.com>
2020-01-13 20:02:28 +0000

    Fix: pep8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,6 +3,6 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    email = {'email' : argv[2]}
+    email = {'email': argv[2]}
     req = requests.post(argv[1], data=email)
     print(req.text)
