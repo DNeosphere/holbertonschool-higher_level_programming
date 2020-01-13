d276a7534be4361b35e9337e65808d7112f656eb
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-13 18:20:55 +0000

    Feature: Task 1, request ID

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,6 @@ from sys import argv
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io') as resp:
+    with request.urlopen(argv[1]) as resp:
         info = resp.info()
         print(info['X-Request-Id'])
