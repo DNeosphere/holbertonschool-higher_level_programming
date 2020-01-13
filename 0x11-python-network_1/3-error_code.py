39932c9255ffc69abbb31e296a9a901aa44c372a
dneosphere <daniela.gomezp05@outlook.com>
2020-01-13 19:17:02 +0000

    Feature: error codes

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib
+from urllib import request
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as resp:
+            info = resp.read()
+            print(info.decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print('Error Code {}'.format(err.code))
