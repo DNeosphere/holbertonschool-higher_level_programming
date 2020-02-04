62bb1087e8f5ceeb8cbe6009a5b6d67489e0b58d
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 19:45:56 +0000

    Fix: semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv.length == 3) {
+if (process.argv.length === 3) {
   console.log('Argument found');
 } else if (process.argv.length > 3) {
   console.log('Arguments found');
