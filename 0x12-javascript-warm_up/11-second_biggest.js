dafc22160d5709bf747559615e54236c054cc5a9
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 22:25:29 +0000

    Fix: js is hell

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,8 +6,10 @@ if (argv.length <= 3) {
   console.log(0);
 } else {
   const nArray = argv.slice(2, argv.length);
-  nArray.sort();
+
+  nArray.sort(function (a, b) { return a - b; });
   const uArray = [...new Set(nArray)];
+  console.log(uArray);
   uArray.pop();
   console.log(uArray[uArray.length - 1]);
 }
