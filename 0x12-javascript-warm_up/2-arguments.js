3839b4c44b089a3f2a405b84df5da8d33d915484
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 19:45:07 +0000

    Fix: logiv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length > 2) {
+if (process.argv.length == 3) {
   console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
