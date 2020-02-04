c07704a8a09a340c80ca50aaac4eeb960f3818c0
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 20:10:14 +0000

    Feature: is a number fx

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const intNumber = parseInt(argv[2]);
+
+if (isNaN(intNumber)) {
+  console.log('Not a number');
+} else {
+  console.log(intNumber);
+}
