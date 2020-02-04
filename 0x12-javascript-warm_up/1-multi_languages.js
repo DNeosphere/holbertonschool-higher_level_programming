deea6bedac87537b4eedbab1666b8e151f88e656
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 19:28:07 +0000

    Fix: semistandard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-arr = ["C is fun", "Python is cool", "Javascript is amazing"]
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (const i in arr) {
-  console.log(arr[i])
+  console.log(arr[i]);
 }
