174759cfa85aa01f19fc4876ac0298ab3fdb2383
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 19:56:42 +0000

    Fix: no length used

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv.length > 2) {
+let len = 0
+for (const i in process.argv){
+  len++;
+}
+
+if (len > 2) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
