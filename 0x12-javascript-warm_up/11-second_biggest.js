2fb1478269cbdab2200623f60c66bfce178352c9
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 21:50:25 +0000

    Feature: task 11, 2 biggest element of an array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+if (argv.length <= 3) {
+  console.log(1);
+} else {
+  const nArray = argv.slice(2, argv.length);
+  nArray.sort();
+  nArray.pop();
+  console.log(nArray[nArray.length - 1]);
+}
