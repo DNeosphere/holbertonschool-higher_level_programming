eb3ba5c530bb04f36a86d23de48944df14d033c0
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 21:30:35 +0000

    Feature: factorial function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,15 +3,15 @@
 const argv = process.argv;
 const numA = parseInt(argv[2]);
 
-function factorial (n) {
-  if (isNaN(n)) {
-    return 1;
-  } else if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-}
-if (!parseInt(numA)) {
+if (isNaN(numA)) {
+  console.log(1);
+} else {
+  const factorial = (n) => {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  };
   console.log(factorial(numA));
 }
