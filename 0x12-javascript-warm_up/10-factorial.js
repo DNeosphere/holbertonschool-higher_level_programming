53bddbb39067ed3ebff16f3459cba654c9aa68db
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 21:33:31 +0000

    Fix: arrow fx removed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,15 +3,16 @@
 const argv = process.argv;
 const numA = parseInt(argv[2]);
 
+function factorial (n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
 if (isNaN(numA)) {
   console.log(1);
 } else {
-  const factorial = (n) => {
-    if (n === 0) {
-      return 1;
-    } else {
-      return n * factorial(n - 1);
-    }
-  };
   console.log(factorial(numA));
 }
