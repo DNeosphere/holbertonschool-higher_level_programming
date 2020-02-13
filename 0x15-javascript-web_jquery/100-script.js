24e1855face0b36570d92b5bda5a413724464bba
Dani Gomez <daniela.gomezp05@outlook.com>
2020-02-13 17:58:13 -0500

    Feature: first advanced in eages

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    console.log('DOM fully loaded and parsed');
+
+    document.querySelector('header').style.color = 'red';
+
+    });
+
